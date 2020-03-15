c500c4fe7d8dad85cdd7f1f223ea7a6f2a8c5423
James Benjamin <james.abenj@gmail.com>
2020-03-15 00:57:49 +0000

    Automatically backed up by Learn

--- app/models/members.rb
+++ app/models/members.rb
@@ -1,9 +1,11 @@
 class Members
   attr_accessor :name, :power, :bio
 
+  @@all = []
   def initialize(name, power, bio)
     @name = name
     @power = power
     @bio = bio
+    @@all << self
   end
 end
